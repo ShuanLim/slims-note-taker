ffa962b737173a04766647c4fb06b36fdcbd13b8
s.limguy <shuanmlim@icloud.com>
2022-07-20 20:24:05 -0700

    get path

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { dirname } = require("path");
 const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8')); 
 
 let app = express();
@@ -14,3 +15,12 @@ var createId = () => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./public/'));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pubic/notes.html"));
+});
